a8717f79eea09a60cfdad79101efee90d4448238
muhammad misbahudin <168268876+misbahudin0812@users.noreply.github.com>
2024-12-25 17:44:12 +0700

    Time: 0 ms (100%), Space: 48.4 MB (93.15%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -3,34 +3,34 @@
  * @param {string} needle
  * @return {number}
  */
-const strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
-};
-
-// // cara manual
+ 
 // const strStr = function(haystack, needle) {
-//     // Jika needle adalah string kosong, kembalikan 0
-//     if (needle === "") return 0;
-
-//     // Panjang haystack dan needle
-//     const haystackLength = haystack.length;
-//     const needleLength = needle.length;
-
-//     // Iterasi melalui haystack untuk mencari kecocokan
-//     for (let i = 0; i <= haystackLength - needleLength; i++) {
-//         let isMatch = true;
-
-//         // Cek setiap karakter di dalam needle
-//         for (let j = 0; j < needleLength; j++) {
-//             if (haystack[i + j] !== needle[j]) {
-//                 isMatch = false;
-//                 break;
-//             }
-//         }
-
-//         if (isMatch) return i; // Jika cocok, kembalikan indeks
-//     }
-
-//     return -1; // Jika tidak ditemukan
+//     return haystack.indexOf(needle);
 // };
 
+// cara manual
+const strStr = function(haystack, needle) {
+    // Jika needle adalah string kosong, kembalikan 0
+    if (needle === "") return 0;
+
+    // Panjang haystack dan needle
+    const haystackLength = haystack.length;
+    const needleLength = needle.length;
+
+    // Iterasi melalui haystack untuk mencari kecocokan
+    for (let i = 0; i <= haystackLength - needleLength; i++) {
+        let isMatch = true;
+
+        // Cek setiap karakter di dalam needle
+        for (let j = 0; j < needleLength; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                isMatch = false;
+                break;
+            }
+        }
+
+        if (isMatch) return i; // Jika cocok, kembalikan indeks
+    }
+
+    return -1; // Jika tidak ditemukan
+};
